07ce1cd69b16b5f969e47247ea945c6ad530d8f9
miikepine <miikepiine@gmail.com>
2023-06-07 00:51:01 +0200

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ["links.papareact.com", "a0.muscache.com"],
+    
+    },
+};
+
+module.exports = nextConfig
+
---
+++ src/app/page.js
@@ -0,0 +1,67 @@
+import Header from "../../components/Header"
+import Banner from "../../components/Banner"
+import SmallCard from "../../components/SmallCard";
+import MediumCards from "../../components/MediumCard";
+import LargeCard from "../../components/LargeCard";
+import Footer from "../../components/Footer";
+
+export default async function Home() {
+  const exploreData = await fetch("https://www.jsonkeeper.com/b/4G1G").
+  then((res) => res.json())
+  .catch(err => console.log(err))
+  ;
+
+
+  const cardsData = await fetch("https://www.jsonkeeper.com/b/VHHT").
+  then((res) => res.json())
+  .catch(err => console.log(err))
+  ;
+
+  return (
+
+<div>
+        <Header/>
+        <Banner />
+
+        <main className="max-w-7xl mx-auto px-8  sm:px-16 ">
+          <section>
+            <h2 className="text-4xl font-semibold pb-5 pt-6" >Explore Nearby</h2>
+
+
+            {/* pull some data from a server - api endpoints */}
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+                  {exploreData?.map(({img, distance, location}) => (
+                    <SmallCard 
+                              key={img}
+                              img={img} 
+                              distance={distance} 
+                              location={location} 
+                              />
+                  ))}
+            </div>
+          </section>
+
+          <section>
+
+            <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
+
+                  <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3">
+                        {cardsData?.map(({img, title}) => (
+                          <MediumCards key={img} img={img} title={title}/>
+                        ))}
+                    </div>
+          </section>
+
+          <LargeCard img="https://a0.muscache.com/im/pictures/2da67c1c-0c61-4629-8798-1d4de1ac9291.jpg?im_w=1440" 
+          title="The Greatest Outdoor" 
+          description="Wishlist created by Airbnb"
+          buttonText="Get Inspired" />
+        </main>
+    
+
+
+    <Footer />
+</div>
+  );
+}
+ 
